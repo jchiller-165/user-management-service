d7f3d58e4a735da1b4dfa6119025da05417bdc1e
Justin Hiller <Justin.Hiller@Hilton.com>
2025-07-01 10:11:04 -0500

    Clean Project

--- src/main/java/com/techservices/usermanagement/errors/exceptions/AppErrors.java
+++
@@ -1,7 +0,0 @@
-package com.techservices.usermanagement.errors.exceptions;
-
-public class AppErrors {
-
-  public static final String NOT_FOUND = "Not Found";
-
-}
--- src/main/java/com/techservices/usermanagement/errors/exceptions/FailureToUpdateException.java
+++
@@ -1,9 +0,0 @@
-package com.techservices.usermanagement.errors.exceptions;
-
-public class FailureToUpdateException extends RuntimeException {
-
-  public FailureToUpdateException(String message) {
-    super(message);
-  }
-
-}
